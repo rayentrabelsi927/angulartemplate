3fb975e8d86d22f4effa3c671b8c160a6b6f5cdf
rayentrabelsi927 <trabelsi.rayen@esprit.tn>
2024-04-15 16:12:46 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './Client/index/index.component';
+import { IndexAdminComponent } from './Admin/index-admin/index-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, //default route
+  { path: 'home', component: IndexComponent },
+  { path: 'admin', component: IndexAdminComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './Client/index/index.component';
+import { IndexAdminComponent } from './Admin/index-admin/index-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    IndexAdminComponent
   ],
   imports: [
     BrowserModule,
